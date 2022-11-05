cbf071e3a98c16099a8d975f262874d08684a70f
DidioDev <lucassmaniottoo@gmail.com>
2022-11-05 03:21:50 -0300

    Adição de imagens

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import Logo from '../src/images/logo.png';
+import Didio from '../src/images/didio.png';
+import Henrique from '../src/images/henrique.png';
+import Kaua from '../src/images/kaua.png';
+import Thalia from '../src/images/thalia.png';
+import Thales from '../src/images/thales.png';
 import './App.css';
 
 function App() {
@@ -8,27 +13,32 @@ function App() {
       <img src={Logo} class="logo" width="100px" height="100px"></img>
       <h1 class="titulo_integrantes">INTEGRANTES DO GRUPO:</h1>
       </div> <br/>
-  
+
       <div class="fotos_integrantes">
           
           <div>
-              <img src="../atividade01/imagens/lucas_smaniotto.jpeg" class="foto_lucas"></img>
-              <h2 class="nome_lucas">Lucas Smaniotto</h2>
+              <img src={Didio} class="foto_didio"></img>
+              <h2 class="nome_didio">Lucas Smaniotto</h2>
           </div>
   
           <div>
-              <img src="../atividade01/imagens/kauã_ribeiro.jpeg" class="foto_kaua"></img>
+              <img src={Henrique} class="foto_henrique"></img>
+              <h2 class="nome_henrique">Henrique Wagner</h2>
+          </div>
+  
+          <div>
+              <img src={Kaua} class="foto_kaua"></img>
               <h2 class="nome_kaua">Kauã de Moura</h2>
           </div>
   
           <div>
-              <img src="../atividade01/imagens/arthur_ofoisch.jpeg" class="foto_arthur"></img>
-              <h2 class="nome_arthur">Arthur Ofoish</h2>
+              <img src={Thalia} class="foto_thalia"></img>
+              <h2 class="nome_thalia">Thalía Goulart</h2>
           </div>
-  
+
           <div>
-              <img src="../atividade01/imagens/lucas_maranhão.jpeg" class="foto_maranhao"></img>
-              <h2 class="nome_maranhao">Lucas Rocha</h2>
+              <img src={Thales} class="foto_thales"></img>
+              <h2 class="nome_thales">Thales Girotto</h2>
           </div>
   
       </div>
